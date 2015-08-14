121470ee48e09177b5db247cad2b727693b41efd
KUAN-YING CHOU <kuanyingchou@gmail.com>
2015-08-14 14:24:23 -0700

    add same tree test

--- Solution.java
+++ Solution.java
@@ -2089,6 +2089,13 @@ public class Solution {
         assertEquals(isSymmetricIteration(root2), false);
     }
 
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null ^ q==null) return false;
+        else if(p==null) return true;
+        else if(p.val != q.val) return false;
+        else return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
     public static void main(String[] args) {
         testSingleNumber();
         testInvertTree();
