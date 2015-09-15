798266e8a3fdd7c3792106a8dfe588193fc54490
KUAN-YING CHOU <kuanyingchou@gmail.com>
2015-09-14 23:14:36 -0700

    add plusOne

--- Solution.java
+++ Solution.java
@@ -2585,6 +2585,22 @@ public class Solution {
         }
         return head;
     }
+    public static int[] plusOne(int[] digits) {
+        for(int i=digits.length-1; i>=0; i--) {
+            if(digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }    
+        }
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        for(int i=0; i<digits.length; i++) {
+            res[i+1] = digits[i];
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
         testSingleNumber();
