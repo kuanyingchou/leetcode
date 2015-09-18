bed87c7c3e43ce614e6540a18ddbf0528520ef73
KUAN-YING CHOU <kuanyingchou@gmail.com>
2015-09-17 22:47:41 -0700

    add max profit 2

--- Solution.java
+++ Solution.java
@@ -2861,6 +2861,17 @@ public class Solution {
         }
         return list;
     }
+    public static int maxProfitII(int[] prices) {
+        int profit = 0;
+        if(prices.length < 2) return 0;
+        for(int i=0; i<prices.length-1; i++) {
+            if(prices[i+1] > prices[i]) {
+                profit += (prices[i+1] - prices[i]);
+                
+            }
+        }
+        return profit;
+    }
 
     public static void main(String[] args) {
         testSingleNumber();
